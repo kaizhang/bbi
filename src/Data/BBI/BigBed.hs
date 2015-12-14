bcbe49eddd2e7912169ec52a15c259316aa991ff
Kai Zhang <kai@kzhang.org>
2015-12-14 11:16:39 -0800

    work on BigBed

--- src/Data/BBI/BigBed.hs
+++ src/Data/BBI/BigBed.hs
@@ -1,24 +1,40 @@
-module Data.BBI.BigBed where
+module Data.BBI.BigBed
+    ( BBedFile
+    , openBBedFile
+    , closeBBedFile
+    , toBedRecords
+    ) where
 
 import qualified Data.ByteString as B
-import Data.Conduit
+import qualified Data.ByteString.Char8 as BC
+import Conduit
 import qualified Data.Conduit.List as CL
 import Data.BBI
 import Data.BBI.Utils
 
-toBedRecords :: Monad m => Endianness -> Conduit B.ByteString m (Int, Int, Int, B.ByteString)
-toBedRecords e = do
-    d <- await
-    case d of
-        Nothing -> return ()
-        Just bs -> go bs
+newtype BBedFile = BBedFile BbiFile
+
+openBBedFile :: FilePath -> IO BWFile
+openBBedFile fl = do
+    bbi <- openBbiFile fl
+    case (_filetype . _header) bbi of
+        BigBed -> return $ BBedFile bbi
+        _ -> error "not a bigbed file"
+
+closeBBedFile :: BBedFile -> IO ()
+closeBBedFile (BBedFile fl) = closeBbiFile fl
+
+
+-- | convert each block to BED records
+toBedRecords :: Monad m
+             => Endianness
+             -> Conduit B.ByteString m (Int, Int, Int, B.ByteString)
+toBedRecords e = concatMapC (map f . B.split '\0')
   where
-    go s | B.null s = return ()
-         | otherwise = do
-             let chr = readInt32 e . B.take 4 $ s
-                 start = readInt32 e . B.take 4 . B.drop 4 $ s
-                 end = readInt32 e . B.take 4 . B.drop 8 $ s
-                 (rest, remain) = B.break (==0) . B.drop 12 $ s
-             yield (chr, start, end, rest)
-             go $ B.tail remain
+    f s = (chr, start, end, rest)
+      where
+        chr = readInt32 e . B.take 4 $ s
+        start = readInt32 e . B.take 4 . B.drop 4 $ s
+        end = readInt32 e . B.take 4 . B.drop 8 $ s
+        rest = B.drop 12 $ s
 {-# INLINE toBedRecords #-}
